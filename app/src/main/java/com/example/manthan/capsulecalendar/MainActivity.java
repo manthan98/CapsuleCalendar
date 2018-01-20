98c5113d805c8cfe9c9ccd027bb0d5a0c46cf487
manthan98 <contact@manthanshah.com>
2018-01-20 15:36:15 -0500

    UI changes

--- app/src/main/java/com/example/manthan/capsulecalendar/MainActivity.java
+++ app/src/main/java/com/example/manthan/capsulecalendar/MainActivity.java
@@ -45,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         mAuth = FirebaseAuth.getInstance();
 
         // Configure Google Sign In
