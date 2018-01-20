a8d377eb24b13c53edb924686f4a223dad9a6b3b
atatmaja <austin.atmaja@gmail.com>
2018-01-20 02:30:17 -0500

    now redirects to calendar app

--- app/src/main/java/com/example/manthan/capsulecalendar/homeScreen.java
+++ app/src/main/java/com/example/manthan/capsulecalendar/homeScreen.java
@@ -1,9 +1,15 @@
 package com.example.manthan.capsulecalendar;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
 
 public class homeScreen extends AppCompatActivity {
 
@@ -16,6 +22,21 @@ public class homeScreen extends AppCompatActivity {
     public void cameraClick(View v){
         Intent i = new Intent(this, camera.class);
         startActivity(i);
+    }
+
+    public void calendarClick(View v){
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(new Date());
+        cal.add(Calendar.MONTH, 2);
+        long time = cal.getTime().getTime();
+        Uri.Builder builder =
+                CalendarContract.CONTENT_URI.buildUpon();
+        builder.appendPath("time");
+        builder.appendPath(Long.toString(time));
+        Intent intent =
+                new Intent(Intent.ACTION_VIEW, builder.build());
+        startActivity(intent);
+
     }
 
 }
